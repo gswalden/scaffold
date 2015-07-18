55f950fc05cce419550411b74e5514b3588c5d15
Greg Walden <greg.walden@makerbot.com>
2015-07-18 12:19:16 -0400

    add bower, editorconfig, stylish

--- gulpfile.js
+++ gulpfile.js
@@ -9,57 +9,60 @@ var notify      = require('gulp-notify');
 var jade        = require('gulp-jade');
 var uglify      = require('gulp-uglify');
 var sourcemaps  = require('gulp-sourcemaps');
+var jshint      = require('gulp-jshint');
 
 // browser-sync task for starting the server.
 gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: './'
-        }
-    });
+  browserSync({
+    server: {
+      baseDir: './'
+    }
+  });
 });
 
-gulp.task('less', function () {
-    return gulp.src('css/src/*.less')
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(autoprefixer())
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('css/dist'))
-        .pipe(reload({stream:true}))
-        .pipe(notify('Finished file: <%= file.relative %>'));
+gulp.task('less', function() {
+  return gulp.src('css/src/*.less')
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('css/dist'))
+    .pipe(reload({ stream: true }))
+    .pipe(notify('Finished file: <%= file.relative %>'))
+  ;
 });
 
-gulp.task('js', function () {
-    return gulp.src('js/src/*.js')
-        .pipe(plumber())
-        // .pipe(jshint())
-        // .pipe(jshint.reporter('default'))
-        .pipe(sourcemaps.init())
-        // .pipe(babel())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('maps'))
-        .pipe(gulp.dest('js/dist'))
-        .pipe(reload({stream:true}))
-        .pipe(notify('Finished file: <%= file.relative %>'));
+gulp.task('js', function() {
+  return gulp.src('js/src/*.js')
+    .pipe(plumber())
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('maps'))
+    .pipe(gulp.dest('js/dist'))
+    .pipe(reload({ stream: true }))
+    .pipe(notify('Finished file: <%= file.relative %>'))
+  ;
 });
 
-gulp.task('jade', function () {
-    return gulp.src('jade/index.jade')
-        .pipe(plumber())
-        .pipe(jade())
-        .pipe(gulp.dest(''))
-        .pipe(reload({stream:true}))
-        .pipe(notify('Finished file: <%= file.relative %>'));
+gulp.task('jade', function() {
+  return gulp.src('jade/index.jade')
+    .pipe(plumber())
+    .pipe(jade())
+    .pipe(gulp.dest(''))
+    .pipe(reload({ stream: true }))
+    .pipe(notify('Finished file: <%= file.relative %>'))
+  ;
 });
 
-gulp.task('bs-reload', function () {
-    browserSync.reload();
+gulp.task('bs-reload', function() {
+  browserSync.reload();
 });
 
 // Default task to be run with `gulp`
-gulp.task('default', ['jade', 'less', 'browser-sync'], function () {
-    gulp.watch('css/src/*.less', ['less']);
-    gulp.watch('jade/index.jade', ['jade']);
-    gulp.watch('js/src/*.js', ['js']);
+gulp.task('default', ['jade', 'less', 'browser-sync'], function() {
+  gulp.watch('css/src/*.less', ['less']);
+  gulp.watch('jade/*.jade', ['jade']);
+  gulp.watch('js/src/*.js', ['js']);
 });
